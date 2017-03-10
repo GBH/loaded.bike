67433557dae3321973f33fa0f8a4146127b3eb99
Oleg <oleg@khabarov.ca>
2017-03-09 20:32:18 -0800

    got crud in, need tests still

--- lib/pedal_app/web/controllers/photo_controller.ex
+++ lib/pedal_app/web/controllers/photo_controller.ex
@@ -6,6 +6,7 @@ defmodule PedalApp.Web.PhotoController do
   plug :scrub_params, "photo" when action in [:create, :update]
   plug :load_tour
   plug :load_waypoint
+  plug :load_photo when action in [:edit, :update, :delete]
 
   defp load_tour(conn, _) do
     %{"tour_id" => tour_id} = conn.params
@@ -17,6 +18,12 @@ defmodule PedalApp.Web.PhotoController do
     %{"waypoint_id" => waypoint_id} = conn.params
     waypoint = Repo.get!(assoc(conn.assigns.tour, :waypoints), waypoint_id)
     assign(conn, :waypoint, waypoint)
+  end
+
+  defp load_photo(conn, _) do
+    %{"id" => id} = conn.params
+    photo = Repo.get!(assoc(conn.assigns.waypoint, :photos), id)
+    assign(conn, :photo, photo)
   end
 
   defp action(conn, _) do
@@ -35,7 +42,7 @@ defmodule PedalApp.Web.PhotoController do
     changeset = Photo.changeset(build_assoc(waypoint, :photos), photo_params)
 
     case Repo.insert(changeset) do
-      {:ok, photo} ->
+      {:ok, _photo} ->
         conn
         |> put_flash(:info, "Photo created")
         |> redirect(to: current_user_tour_waypoint_path(conn, :show, tour, waypoint))
@@ -44,15 +51,31 @@ defmodule PedalApp.Web.PhotoController do
     end
   end
 
-  def edit(conn, %{"id" => id}, tour, waypoint) do
-
+  def edit(conn, %{"id" => _id}, tour, waypoint) do
+    photo = conn.assigns.photo
+    changeset = Photo.changeset(photo)
+    render conn, "edit.html", tour: tour, waypoint: waypoint, photo: photo, changeset: changeset
   end
 
-  def update(conn, %{"id" => id, "photo" => photo_params}, tour, waypoint) do
+  def update(conn, %{"id" => _id, "photo" => photo_params}, tour, waypoint) do
+    photo = conn.assigns.photo
+    changeset = Photo.changeset(photo, photo_params)
 
+    case Repo.update(changeset) do
+      {:ok, _photo} ->
+        conn
+        |> put_flash(:info, "Photo updated")
+        |> redirect(to: current_user_tour_waypoint_path(conn, :show, tour, waypoint))
+      {:error, changeset} ->
+        render(conn, "edit.html", tour: tour, waypoint: waypoint, photo: photo, changeset: changeset)
+    end
   end
 
-  def delete(conn, %{"id" => id}, tour, waypoint) do
+  def delete(conn, %{"id" => _id}, tour, waypoint) do
+    Repo.delete!(conn.assigns.photo)
 
+    conn
+    |> put_flash(:info, "Photo deleted")
+    |> redirect(to: current_user_tour_waypoint_path(conn, :show, tour, waypoint))
   end
 end
