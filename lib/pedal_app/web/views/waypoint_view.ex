0da9c2f3277dd106f42f447e54790585d8a2d498
Oleg <oleg@khabarov.ca>
2017-03-12 23:05:51 -0700

    seed waypoints data

--- lib/pedal_app/web/views/waypoint_view.ex
+++ lib/pedal_app/web/views/waypoint_view.ex
@@ -1,6 +1,7 @@
 defmodule PedalApp.Web.WaypointView do
   use PedalApp.Web, :view
 
+  def markdown(nil), do: ""
   def markdown(body) do
     {_, html, _} = Earmark.as_html(body)
     HtmlSanitizeEx.markdown_html(html) |> raw
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PedalApp.{Repo, Tour, Waypoint}
+json = File.read!("priv/repo/seed_data/waypoints.json")
+
+
+
+data = Poison.Parser.parse!(json)
+
+Enum.each(data, fn(wp) ->
+  tour = Repo.get!(Tour, 5)
+  changeset = Waypoint.changeset(Ecto.build_assoc(tour, :waypoints), %{
+    title:  wp["title"],
+    lat:    wp["lat"],
+    lng:    wp["lng"]
+  })
+  Repo.insert!(changeset)
+end)
