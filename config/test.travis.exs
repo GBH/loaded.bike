f82fdd79aa41d9a2d65a779da035402570897939
Oleg <oleg@khabarov.ca>
2017-04-24 22:26:55 -0700

    how the hell did it work before

---
+++ config/test.travis.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :loaded_bike, LoadedBike.Web.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :loaded_bike, LoadedBike.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "travis_ci_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Speeding up BCrypt things for test env
+config :comeonin, :bcrypt_log_rounds, 1
