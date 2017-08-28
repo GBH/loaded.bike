830d81c6c4724c00e52f383343fd36a81f2f721e
Oleg <oleg@khabarov.ca>
2017-08-28 11:09:49 -0700

    getting there

--- lib/loaded_bike/web/views/helpers/form_helpers.ex
+++ lib/loaded_bike/web/views/helpers/form_helpers.ex
@@ -10,19 +10,49 @@ defmodule LoadedBike.Web.FormHelpers do
     generic_input(:textarea, form, field, opts)
   end
 
+  def radio_button(form, field, value, opts \\ []) do
+    label = Keyword.get(opts, :label, Form.humanize(field))
+    input = Form.radio_button(form, field, value, class: "form-check-input")
+    Tag.content_tag :div, class: "form-check" do
+      Tag.content_tag :label, class: "form-check-label" do
+        [input, "\n", label]
+      end
+    end
+  end
+
+  def checkbox(form, field, opts \\ []) do
+    label = Keyword.get(opts, :label, Form.humanize(field))
+    input = Form.checkbox(form, field, class: "form-check-input")
+    Tag.content_tag :div, class: "form-check" do
+      Tag.content_tag :label, class: "form-check-label" do
+        [input, "\n", label]
+      end
+    end
+  end
+
   def form_group(form, field, opts \\ [], [do: block]) do
     state = state_class(form, field)
+
+    label = Keyword.get(opts, :label, Form.humanize(field))
+    label = Tag.content_tag :label, label, class: "col-sm-2 text-sm-right col-form-label"
+
+    control = Tag.content_tag :div, class: "col-sm-10" do
+      [block, error_tag(form, field) || ""]
+    end
+
     Tag.content_tag :div, class: "form-group row #{opts[:class]} #{state}" do
-      block
+      [label, control]
     end
   end
 
   # -- Private methods ---------------------------------------------------------
 
-  defp generic_input(type, form, field, opts) do
+  defp generic_input(type, form, field, opts) when is_atom(type) do
     label   = label(form, field, opts)
     control = control(form, field, type, opts)
-    form_group(form, field, opts) do
+    state   = state_class(form, field)
+
+    Tag.content_tag :div, class: "form-group row #{state}" do
       [label, control]
     end
   end
@@ -42,13 +72,12 @@ defmodule LoadedBike.Web.FormHelpers do
     input_opts = Keyword.get(opts, :input_opts, [])
     input_opts = Keyword.merge(input_opts, [class: "form-control"])
 
-    input = apply(Phoenix.HTML.Form, field_type, [form, field, input_opts])
+    input = apply(Form, field_type, [form, field, input_opts])
 
     Tag.content_tag :div, control_opts do
       [
         input,
-        error_tag(form, field) || "",
-        opts[:do] || ""
+        error_tag(form, field) || ""
       ]
     end
   end
